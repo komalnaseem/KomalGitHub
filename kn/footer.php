5f83f265098b85f6be0fafa38f47cdb504054e91
komalnaseem <komalnaseem@hotmail.com>
2016-08-05 22:45:45 -0400

    Setting up the site structure and responsive design

--- kn/footer.php
+++ kn/footer.php
@@ -11,16 +11,21 @@
 
 ?>
 
-	</div><!-- #content -->
+			</div><!-- #content -->
+		</div><!-- .main-page-->	
+	</div><!-- .main-content-area full-->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kn' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'kn' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'kn' ), 'kn', '<a href="http://phoenix.sheridanc.on.ca/~ccit3680/" rel="designer">Komal Naseem</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<div class="footer-area full">
+		<div class="main-page">	
+			<footer id="colophon" class="site-footer inner" role="contentinfo">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kn' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'kn' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'kn' ), 'kn', '<a href="http://phoenix.sheridanc.on.ca/~ccit3680/" rel="designer">Komal Naseem</a>' ); ?>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- .main-page -->
+	</div><!-- .footer-area full-->
 
 <?php wp_footer(); ?>
 
