a12883008bede0e58f6003ace3c5767cc4bcdbcc
komalnaseem <komalnaseem@hotmail.com>
2016-08-17 00:08:29 -0400

    More styling and add comments

--- kn/header.php
+++ kn/header.php
@@ -46,15 +46,15 @@
 				</div><!-- .site-branding -->
 				
 					<?php /* This is to display custom fields */
-					$author = get_post_meta ($post->ID, 'author', true);
+					$link = get_post_meta ($post->ID, 'link', true);
 				
 					global $wp_query;
 					$postid = $wp_query->post->ID;
-					$author = get_post_meta($postid, 'author', true);
+					$link = get_post_meta($postid, 'link', true);
 					?>
-	
-					<p> JK Rowling <?php echo $author; ?>.</p>
 					
+					<?php/* <p> <?php echo $link; ?>.</p>  This was used for author that we have changed it to link on home page*/ ?>
+					<p><a href="<?php echo "My Linkedin Profile: " .esc_url( __( $link , 'kn' ) ); ?>"></a>
 					<php wp_reset_query(); ?>
 				
 				<nav id="site-navigation" class="main-navigation" role="navigation">
--- kn/single.php
+++ kn/single.php
@@ -30,7 +30,7 @@ get_header(); ?>
 
 			
 			get_template_part( 'template-parts/content', get_post_format() );
-
+			the_post_thumbnail();
 			the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
